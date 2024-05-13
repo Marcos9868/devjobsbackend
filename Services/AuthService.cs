b5594327205e35ebd10706b2b9a63d30cdfeeb6b
Marcos Melo Ferreira <79291596+Marcos9868@users.noreply.github.com>
2024-05-13 17:51:32 -0300

    Update AuthService.cs
    Fix using position and indent code

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -1,29 +1,22 @@
-﻿
-
-namespace DevJobsBackend.Services{
 using BCrypt.Net;
-
-  
-    using DevJobsBackend.Contracts.Services;
-    using DevJobsBackend.Data;
-
+using DevJobsBackend.Contracts.Services;
+using DevJobsBackend.Data;
+﻿
+namespace DevJobsBackend.Services{
     public class AuthService : IAuthService
     {
-    private readonly DataContext _context;
-
-       
-       
-
+        private readonly DataContext _context;
+        
         public AuthService(DataContext context)
-    {
-        _context = context;
-    }
+        {
+            _context = context;
+        }
 
-    public Task<string> GenerateHashPassword(string password)
-    {
-        string hashedPassword = BCrypt.HashPassword(password);
-
-        System.Diagnostics.Debug.WriteLine(hashedPassword);
-        return Task.FromResult(hashedPassword);
+        public Task<string> GenerateHashPassword(string password)
+        {
+            string hashedPassword = BCrypt.HashPassword(password);
+            System.Diagnostics.Debug.WriteLine(hashedPassword);
+            return Task.FromResult(hashedPassword);
+        }
     }
-}}
+}
