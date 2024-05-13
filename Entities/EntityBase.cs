4ba0d0fa770ffb98e53abefe536a6cba1c090080
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-05-13 18:18:21 -0300

    Feat: add more default parameters for user

--- Entities/EntityBase.cs
+++ Entities/EntityBase.cs
@@ -1,13 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using DevJobsBackend.Enums;
 
 namespace DevJobsBackend.Entities
 {
     public abstract class EntityBase
     {
         public int Id { get; set; }
-        public int TypeUser { get; set; }
+        public UserType TypeUser { get; set; }
+        public string Linkedin { get; set; } = string.Empty;
+        public string Instagram { get; set; } = string.Empty;
+        public List<string> AboutMe { get; set; } = new List<string>();
+        public List<string> Skills { get; set; } = new List<string>();
+        public decimal SalaryExpectations { get; set; } = 0_0;
+        public decimal HourExpectation { get; set; } = 0_0;
+        public bool LegalEntity { get; set; } = false;
+        public string Phone { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
