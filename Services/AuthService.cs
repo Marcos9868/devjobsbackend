b53a3c667cc171c0843071920aa1fd528e296704
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-05-16 17:53:32 -0300

    Feat: add reset password service

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -1,4 +1,7 @@
-﻿using DevJobsBackend.Contracts.Services;
+﻿using System.Security.Cryptography;
+using System.Text;
+using BCrypt.Net;
+using DevJobsBackend.Contracts.Services;
 using DevJobsBackend.Data;
 using DevJobsBackend.Entities;
 
@@ -7,22 +10,44 @@ namespace DevJobsBackend.Services
     public class AuthService : IAuthService
     {
         private readonly DataContext _context;
-
-        public AuthService(DataContext context)
+        private readonly IUserService _userService;
+        public AuthService(DataContext context, IUserService userService)
         {
             _context = context;
+            _userService = userService;
         }
-        public async Task<string> GenerateHashPassword(string password)
+        public string GenerateHashPassword(string password)
         {
-            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
-            return await Task.FromResult(hashedPassword);
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+            // string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+            // return await Task.FromResult(hashedPassword);
         }
         public dynamic RegistrateUser(User user)
         {
+            var hashPassword = GenerateHashPassword(user.HashPassword);
+            user.HashPassword = hashPassword;
             _context.Users.Add(user);
-            return _context.SaveChanges() > 0 
-                ? user 
+            return _context.SaveChanges() > 0
+                ? user
                 : "Unable to registrate user";
+        }
+        public async Task<string> ResetPassword(string Email, string NewPassword)
+        {
+            var user = await _userService.GetUserByEmail(Email) ?? throw new Exception("Unable to find user");
+            var newHashedPassword = BCrypt.Net.BCrypt.HashPassword(NewPassword) ?? throw new Exception("Unable to hash password");
+            user.HashPassword = newHashedPassword;
+            _context.SaveChanges();
+            return "Password changed with success";
         }
     }
 }
