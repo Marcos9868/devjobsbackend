5ef8d4952e01cd8d5948bec708f877c5428a2e31
Lucas Fonseca <lucas.ribeiro.24444@gmail.com>
2024-05-22 21:11:35 +0100

    fix: Some Services Changed

--- Contracts/Services/IUserService.cs
+++ Contracts/Services/IUserService.cs
@@ -5,8 +5,8 @@ namespace DevJobsBackend.Contracts.Services
     public interface IUserService
     {
         Task<List<User>> GetUsers();
+        Task<List<User>> GetMe(string JwtToken);
         Task<User> GetUser(int idUser);
-        Task<User> GetUserByEmail(string userEmail);
         Task<User> AddUser(User user);
         Task<string> UpdateUser(User user);
         Task<string> RemoveUser(User user);
