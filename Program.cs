af8b13f3e44975d3e5fe43f02e4459e8a3c75dea
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-05-28 16:30:23 -0300

    Feat: WebHost and AddSwaggenGen

--- Program.cs
+++ Program.cs
@@ -2,12 +2,13 @@ using DevJobsBackend.IoC.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://*:5000");
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 ServicesMapping.AddAuthentication(builder.Services, builder, builder.Configuration);
 ServicesMapping.AddDbContext(builder.Services, builder);
 ServicesMapping.AddServices(builder.Services);
+ServicesMapping.AddSwagger(builder.Services);
 
 var app = builder.Build();
 
@@ -15,6 +16,10 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "DevJobsBackend v1");
+    });
 }
 
 // app.UseHttpsRedirection();
