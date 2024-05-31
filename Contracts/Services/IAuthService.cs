033b1684b4b3d5a14997c98f4109146773fbe82b
Lucas Fonseca <lucas.ribeiro.24444@gmail.com>
2024-05-31 23:29:23 +0100

    fix: added ValidateForgotPasswordTokenAndGetEmail on  interface and GenerateHashPassword is public now

--- Contracts/Services/IAuthService.cs
+++ Contracts/Services/IAuthService.cs
@@ -5,12 +5,15 @@ using DevJobsBackend.Responses;
 namespace DevJobsBackend.Contracts.Services;
 
 public interface IAuthService
-{    Task<ResponseBase<TokenResponse>> Login(LoginDTO loginDTO);
-    
+{
+    Task<ResponseBase<TokenResponse>> Login(LoginDTO loginDTO);
+
     ResponseBase<TokenResponse> GenerateAccessTokenResponse(string refreshToken);
     Task<ResponseBase<User>> RegistrateUser(User user);
 
     Task<ResponseBase<string>> ForgotPassword(string Email);
 
+    string GenerateHashPassword(string password);
+    string ValidateForgotPasswordTokenAndGetEmail(string jwtToken);
 
 }
