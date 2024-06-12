5e2cc82f42f6c55b2bd409a296d8eae3b42a353d
Lucas Fonseca <lucas.ribeiro.24444@gmail.com>
2024-06-12 20:26:06 +0100

    fix: admin anotation refactored

--- annotations/AdminAnotation.cs
+++ annotations/AdminAnotation.cs
@@ -1,29 +1,66 @@
-﻿using DevJobsBackend.Entities;
+﻿using DevJobsBackend.Contracts.Services;
 using DevJobsBackend.Enums;
+using DevJobsBackend.Responses;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
 public class AdminAttribute : Attribute, IAsyncActionFilter
 {
     public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
     {
-        // Obtém o CurrentUser do contexto
-        if (context.HttpContext.Items.TryGetValue("CurrentUser", out var currentUserObj) && currentUserObj is User currentUser)
+        var authService = context.HttpContext.RequestServices.GetService<IAuthService>();
+        var authorizationHeader = context.HttpContext.Request.Headers["Authorization"].FirstOrDefault();
+
+        if (string.IsNullOrEmpty(authorizationHeader) || !authorizationHeader.StartsWith("Bearer "))
         {
-            if ( currentUser.TypeUser == UserType.ADMIN)
+            SetUnauthorizedResult(context, "Authorization header is missing or invalid.");
+            return;
+        }
+
+        var token = authorizationHeader.Substring("Bearer ".Length).Trim();
+
+        try
+        {
+            var user = await authService.GetUserByAccessToken(token);
+
+            if (user != null && user.TypeUser == UserType.ADMIN)
             {
                 await next();
             }
             else
             {
-                context.Result = new ForbidResult();
+                SetForbiddenResult(context, "Access denied. You do not have the required permissions.");
             }
         }
-        else
+        catch
         {
-            context.Result = new UnauthorizedResult();
+            SetUnauthorizedResult(context, "Invalid token or user not authenticated.");
         }
+    }
+
+    private void SetUnauthorizedResult(ActionExecutingContext context, string message)
+    {
+        var response = new ResponseBase<object>
+        {
+            Data = null,
+            Message = message,
+            Status = false
+        };
+        context.Result = new JsonResult(response) { StatusCode = 401 }; // Unauthorized
+    }
+
+    private void SetForbiddenResult(ActionExecutingContext context, string message)
+    {
+        var response = new ResponseBase<object>
+        {
+            Data = null,
+            Message = message,
+            Status = false
+        };
+        context.Result = new JsonResult(response) { StatusCode = 403 }; // Forbidden
     }
 }
